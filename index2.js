9d8273c2a6a624784c2584645e5e06bf1c5faf52
anandifrdnsyh <anandijr17@gmail.com>
2017-07-20 14:45:03 +0700

    html1
    1

---
+++ index2.js
@@ -0,0 +1,14 @@
+function popup(){
+
+	var overlay=document.getElementsByClassName('overlay');
+
+	overlay[0].style.display='block';
+}
+
+function closed() {
+var overlay=document.getElementsByClassName('overlay');
+
+overlay[0].style.display='none';
+
+
+}
